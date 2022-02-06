45f17536724a698e32bace5a8aa51c2efbd6da51
Holger Winter <winter@tuhh.de>
2022-02-06 16:12:43 +0100

    Fehler in Maussteuerung behoben

--- Seekarte.gd
+++ Seekarte.gd
@@ -3,37 +3,33 @@ extends Node2D
 const EinGrad:float = PI/180
 
 var z_windrichtung:Sprite
-var windrichtung:float
-var z_hscrollbar:HScrollBar
-var z_vscrollbar:VScrollBar
+var z_sb_ruder:HScrollBar
+var z_sb_segel:VScrollBar
 var z_Kamera:Camera2D
 var z_hud:Control
+var z_boot: KinematicBody2D
+var z_cam: Camera2D
 
-var bootBody: KinematicBody2D
-var cam: Camera2D
 var zoom:int = 1
-
+var windrichtung:float
 var maus_sichtbar:bool=true
 var old_maus_pos:Vector2
-var mittelpunkt:Vector2
-var hscroll_bereich_halbe:float
-var vscroll_bereich_halbe:float
-var hscroll_multi:float
-var vscroll_multi:float
+var proz_fenster:Vector2
+var proz_segel:float
+var proz_ruder:float
 
 func _ready():
-	print(get_viewport_rect().size)
 	z_windrichtung = get_node("Camera2D/HUD/Sp_Windrichtung")
-	z_hscrollbar = get_node("Camera2D/HUD/HScrollBar")
-	z_vscrollbar = get_node("Camera2D/HUD/VScrollBar")
+	z_sb_ruder = get_node("Camera2D/HUD/HScrollBar")
+	z_sb_segel = get_node("Camera2D/HUD/VScrollBar")
 	z_Kamera = get_node("Camera2D")
 	z_hud = get_node("Camera2D/HUD")
-	bootBody = get_node("Boot/KinematicBody2D")
-	hscroll_bereich_halbe = ((z_hscrollbar.max_value - z_hscrollbar.min_value) / 2)
-	vscroll_bereich_halbe = ((z_vscrollbar.max_value - z_vscrollbar.min_value) / 2)
-
+	z_boot = get_node("Boot/KinematicBody2D")
+	proz_segel = (z_sb_segel.max_value - z_sb_segel.min_value) / 100
+	proz_ruder = (z_sb_ruder.max_value - z_sb_ruder.min_value) / 100
+	
 func _process(delta):
-	z_Kamera.global_position = bootBody.global_position
+	z_Kamera.global_position = z_boot.global_position
 
 func set_windrichtung():
 	if windrichtung > PI:
@@ -48,23 +44,19 @@ func steuerung_aktiv():
 		maus_sichtbar = false
 		# ermittel die aktuelle Mauspos
 		old_maus_pos = get_viewport().get_mouse_position()
-		# ermittel Fenstergröße und teile sie durch 2
-		mittelpunkt = get_viewport_rect().size / 2
-		# setze Mauszeiger in die mitte des Fensters
 		################## mauszeiger auf werte der beiden Potis setzen
 		var dummi:Vector2
-		vscroll_multi = (get_viewport_rect().size.y) / (z_vscrollbar.max_value - z_vscrollbar.min_value + 1)
-		dummi.y = ((z_vscrollbar.value - z_vscrollbar.min_value + 1) * vscroll_multi) - mittelpunkt.y
-		hscroll_multi = get_viewport_rect().size.x / (z_hscrollbar.max_value - z_hscrollbar.min_value)
-		dummi.x = (z_hscrollbar.value - z_hscrollbar.min_value) * hscroll_multi
-		get_viewport().warp_mouse(dummi)
+		proz_fenster = get_viewport_rect().size / 100
+		var x = (z_sb_ruder.value / proz_ruder + 50) * proz_fenster.x
+		var y = (z_sb_segel.value - z_sb_segel.min_value) / proz_segel * proz_fenster.y
+		get_viewport().warp_mouse(Vector2(x,y))
 		# Mauszeiger unsichtbar machen
-		#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func steuerung_deaktiv():
 	maus_sichtbar = true
 	get_viewport().warp_mouse(Vector2(old_maus_pos))
-	#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _input(event):
 	if event.is_action_pressed("test_wind_plus",true):
@@ -74,12 +66,12 @@ func _input(event):
 		windrichtung = z_windrichtung.rotation - EinGrad
 		set_windrichtung()
 	if event.is_action_pressed("links",true):
-		z_hscrollbar.value -= 0.05
+		z_sb_ruder.value -= 0.05
 	if event.is_action_pressed("oben"):
 		pass
 		#print("Aktion oben")
 	if event.is_action_pressed("rechts",true):
-		z_hscrollbar.value += 0.05
+		z_sb_ruder.value += 0.05
 	if event.is_action_pressed("unten"):
 		pass
 		#print("Aktion unten")
@@ -104,19 +96,8 @@ func _input(event):
 			else:
 				steuerung_deaktiv()
 	if event is InputEventMouseMotion and maus_sichtbar == false:
-		var dummi = ((event.position - mittelpunkt) / mittelpunkt)
-		z_hscrollbar.value = dummi.x * hscroll_bereich_halbe
-		z_vscrollbar.value = (dummi.y + mittelpunkt.y) * vscroll_bereich_halbe
-	print(event.position)
-	print(get_viewport().get_mouse_position())
-	print("-")
-
-func _notification(what):
-	match what:
-		NOTIFICATION_WM_MOUSE_EXIT:
-			if maus_sichtbar == false:
-				steuerung_deaktiv()
-			print("Maus im Fenster")
-		NOTIFICATION_WM_MOUSE_ENTER:
-
-			print("Maus ausserhalb des Fensters")
+		var x = clamp(get_viewport().get_mouse_position().x, 0, get_viewport_rect().size.x)
+		var y = clamp(get_viewport().get_mouse_position().y, 0, get_viewport_rect().size.y)
+		proz_fenster = get_viewport_rect().size / 100 # größe vom fenster kann sich ändern
+		z_sb_segel.value = z_sb_segel.min_value + (y / proz_fenster.y) * proz_segel
+		z_sb_ruder.value = z_sb_ruder.min_value + (x / proz_fenster.x) * proz_ruder
